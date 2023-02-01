c652ecfb1e07f1fe058f525afb8c4075915c0fb8
Jeffrey <1115378579@qq.com>
2023-02-01 14:27:27 +0800

    commit

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
 
+# upload-files-to-oss
+- 上传指定文件夹内所有文件到oss
 
+## 使用
+```js
+// esm
+
+import uploadFileToOSS from 'upload-files-to-oss'
+uploadFileToOSS(
+  {
+    region: 'xxx',
+    accessKeyId: 'xxx',
+    accessKeySecret: 'xxx',
+    bucket: 'xxx'
+  },
+  // '文件夹路径'
+  '../dist'
+)
+// ejs
+const  uploadFileToOSS = require('upload-files-to-oss')
+uploadFileToOSS(
+  {
+    region: 'xxx',
+    accessKeyId: 'xxx',
+    accessKeySecret: 'xxx',
+    bucket: 'xxx'
+  },
+  // '文件夹路径'
+  '../dist'
+)
+```
 ## License
 
 [MIT](./LICENSE) License © 2022 [Jeffrey-mu](https://github.com/Jeffrey-mu)
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-export const one = 1
-export const two = 2
+import OSS from 'ali-oss'
+import path from 'path'
+import fs from 'fast-glob'
+type Option = {
+  region: string,
+  accessKeyId: string,
+  accessKeySecret: string,
+  bucket: string
+}
+export default async function uploadFileToOSS(option: Option, localFileAddress: string) {
+  let client = new OSS(
+    option
+  );
+  const docs = fs.sync(localFileAddress + '/**')
+  let fileTotal = docs.length;
+  docs.forEach(async (file, index) => {
+    await client.put(file.split(localFileAddress.split('/').at(-1) as string)[1].slice(1), path.normalize(file));
+    console.log(`文件上传中:${fileTotal} /${index + 1}`);
+  })
+}
